24f446b71db537d94e7079a91a3227d9646a5e68
Taner Tombaş <tombastaner@gmail.com>
2017-04-30 19:10:15 +0300

    kategori sistemi veri tabanı eklendi.

--- db/deltaajans.sql
+++ db/deltaajans.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 29 Nis 2017, 14:29:28
+-- Üretim Zamanı: 30 Nis 2017, 16:09:35
 -- Sunucu sürümü: 5.7.14
 -- PHP Sürümü: 5.6.25
 
@@ -102,16 +102,96 @@ INSERT INTO `pages` (`page_id`, `page_name`, `page_description`, `page_text`, `a
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `product_categori`
+--
+
+CREATE TABLE `product_categori` (
+  `categori_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `categori_name` varchar(255) NOT NULL,
+  `categori_seo` varchar(255) NOT NULL,
+  `categori_description` text NOT NULL,
+  `categori_image` varchar(150) NOT NULL,
+  `stats` int(11) NOT NULL DEFAULT '0',
+  `categori_sira` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `product_categori`
+--
+
+INSERT INTO `product_categori` (`categori_id`, `group_id`, `categori_name`, `categori_seo`, `categori_description`, `categori_image`, `stats`, `categori_sira`) VALUES
+(1, 1, 'Yönetici Grupları', 'yonetici-gruplari', '', '', 1, 0),
+(2, 1, 'Çalışma Grupları', 'calisma-gruplari', 'Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli \'buraya metin gelecek, buraya metin gelecek\' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında \'lorem ipsum\' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir.', '', 1, 0),
+(3, 1, 'Toplantı Masaları', 'toplanti-masalari', '', '', 1, 0),
+(5, 1, 'Dolaplar &amp; Kitaplıklar', 'dolaplar-kitapliklar', '', '', 1, 0),
+(6, 1, 'Oturma Grupları', 'oturma-gruplari', '', '', 1, 0),
+(7, 1, 'İlave Modüller', 'ilave-moduller', '', '', 1, 0),
+(8, 2, 'Okul Sıraları', 'okul-siralari', '', '', 1, 0),
+(9, 2, 'Amfi Sıraları', 'amfi-siralari', '', '', 1, 0),
+(10, 2, 'Öğretmen Kürsüleri', 'ogretmen-kursuleri', '', '', 1, 0),
+(11, 2, 'Sınıf Dolapları', 'sinif-dolaplari', '', '', 1, 0),
+(12, 2, 'Kitaplıklar', 'kitapliklar', '', '', 1, 0),
+(13, 2, 'Diğer Ürünler', 'diger-urunler', '', '', 1, 0),
+(15, 3, 'Malzeme &amp; Takım Dolapları', 'malzeme-takim-dolaplari', '', '', 1, 0),
+(16, 3, 'Takım Arabaları', 'takim-arabalari', '', '', 1, 0),
+(17, 3, 'Çalışma Tezgahları', 'calisma-tezgahlari', '', '', 1, 0),
+(18, 4, 'Compact Arşiv Sistemleri', 'compact-arsiv-sistemleri', '', '', 1, 0),
+(19, 4, 'Raf Sistemleri', 'raf-sistemleri', '', '', 1, 0),
+(20, 5, 'Karyola &amp; Ranzalar', 'karyola-ranzalar', '', '', 1, 0),
+(21, 5, 'Elbise Dolapları', 'elbise-dolaplari', '', '', 1, 0),
+(22, 5, 'İlave Modüller', 'ilave-moduller', '', '', 1, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `product_group`
 --
 
 CREATE TABLE `product_group` (
   `group_id` int(11) NOT NULL,
   `group_name` varchar(255) NOT NULL,
+  `group_seo` varchar(255) NOT NULL,
   `group_image` varchar(150) NOT NULL,
   `stats` int(11) NOT NULL DEFAULT '0',
   `group_sira` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `product_group`
+--
+
+INSERT INTO `product_group` (`group_id`, `group_name`, `group_seo`, `group_image`, `stats`, `group_sira`) VALUES
+(1, 'Ofis Mobilyaları', 'ofis-mobilyalari', '', 1, 0),
+(2, 'Eğitim Mobilyaları', 'egitim-mobilyalari', '', 1, 0),
+(3, 'Endüstriyel Ekipmanlar', 'endustriyel-ekipmanlar', '', 1, 0),
+(4, 'Depolama &amp; Arşiv', 'depolama-arsiv', '', 1, 0),
+(5, 'Yurt &amp; Konaklama', 'yurt-konaklama', '', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `product_subcategori`
+--
+
+CREATE TABLE `product_subcategori` (
+  `subcategori_id` int(11) NOT NULL,
+  `categori_id` int(11) NOT NULL,
+  `subcategori_name` varchar(255) NOT NULL,
+  `subcategori_seo` varchar(255) NOT NULL,
+  `subcategori_description` text NOT NULL,
+  `subcategori_image` varchar(150) NOT NULL,
+  `stats` int(11) NOT NULL DEFAULT '0',
+  `subcategori_sira` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `product_subcategori`
+--
+
+INSERT INTO `product_subcategori` (`subcategori_id`, `categori_id`, `subcategori_name`, `subcategori_seo`, `subcategori_description`, `subcategori_image`, `stats`, `subcategori_sira`) VALUES
+(1, 1, 'ALFA', 'alfa', '', '', 1, 0),
+(2, 1, 'STYLE', 'style', '', '', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -174,10 +254,22 @@ ALTER TABLE `pages`
   ADD PRIMARY KEY (`page_id`);
 
 --
+-- Tablo için indeksler `product_categori`
+--
+ALTER TABLE `product_categori`
+  ADD PRIMARY KEY (`categori_id`);
+
+--
 -- Tablo için indeksler `product_group`
 --
 ALTER TABLE `product_group`
   ADD PRIMARY KEY (`group_id`);
+
+--
+-- Tablo için indeksler `product_subcategori`
+--
+ALTER TABLE `product_subcategori`
+  ADD PRIMARY KEY (`subcategori_id`);
 
 --
 -- Tablo için indeksler `sub_pages`
@@ -211,10 +303,20 @@ ALTER TABLE `img_library`
 ALTER TABLE `pages`
   MODIFY `page_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- Tablo için AUTO_INCREMENT değeri `product_categori`
+--
+ALTER TABLE `product_categori`
+  MODIFY `categori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+--
 -- Tablo için AUTO_INCREMENT değeri `product_group`
 --
 ALTER TABLE `product_group`
-  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- Tablo için AUTO_INCREMENT değeri `product_subcategori`
+--
+ALTER TABLE `product_subcategori`
+  MODIFY `subcategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Tablo için AUTO_INCREMENT değeri `sub_pages`
 --
